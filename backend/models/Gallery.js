ca044266f64790a0becc5ae842e73cc2a035d5b4
Federico Russo <38754521+Jodolf@users.noreply.github.com>
2025-02-07 15:57:44 +0100

    Aggiunto al modello gelleria eventuake latitudine e longitudine

--- backend/models/Gallery.js
+++ backend/models/Gallery.js
@@ -1,11 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const gallerySchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
   location: { type: String, required: true },
+  /*latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },*/
   images: [String],
 });
 
-const Gallery = mongoose.model('Gallery', gallerySchema);
+const Gallery = mongoose.model("Gallery", gallerySchema);
 export default Gallery; // Esportiamo il modello
